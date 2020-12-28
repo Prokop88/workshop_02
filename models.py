5059eccf4633e1b37a7857747edebb8194b1efaa
Piotr Ert Prokop <pprokop88@gmail.com>
2020-12-28 21:11:54 +0100

    create class messages, creater clcrypto

--- models.py
+++ models.py
@@ -73,5 +73,49 @@ class User:
             return loaded_user
 
 
-class Messages():
-    pass
+class Message:
+    def __init__(self, from_id, to_id, text):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self._creation_date = None
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def creation_date(self):
+        return self._creation_date
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """ INSERT INTO Message(from_id, to_id, text) VALUES(%S, %s, %s) 
+                    RETURNING id = %s, creation_date = %s"""
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            self._id, self._creation_date = cursor.fetchone()
+            return True
+        else:
+            sql = """ UPDATE Message SET to_id = %s, from_id = %s, text=%S WHERE id=%s"""
+            values = (self.to_id, self.from_id, self.text, self.id)
+            cursor.execute(sql, values)
+            return True
+
+    @staticmethod
+    def load_all_messages(cursor, user_id=None):
+        if user_id:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages WHERE to_id=%s"
+            cursor.execute(sql, (user_id,))  # (user_id, ) - cause we need a tuple
+        else:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages"
+            cursor.execute(sql)
+        messages = []
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Message(from_id, to_id, text)
+            loaded_message._id = id_
+            loaded_message._creation_date = creation_date
+            messages.append(loaded_message)
+        return messages
